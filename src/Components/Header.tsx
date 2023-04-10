f7c2eb075b2920418a43fca84557daa4a809bc20
southatelove <artem.jlez@mail.ru>
2023-04-11 00:22:27 +0300

    first commit

---
+++ src/Components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <section className="header-section">
+      <div className="header-section__content-container container">
+        <a></a>
+      </div>
+    </section>
+  );
+}
+export default Header;
---
+++ src/Components/MainBlock.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MainBlock() {
+  return (
+    <section className="main-section">
+      <div className="main-section__content-container container">
+        I'am" MainBlock
+      </div>
+    </section>
+  );
+}
+export default MainBlock;
